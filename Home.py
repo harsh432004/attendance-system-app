e87ca8db76f43254cc33d58cd200125f72809e7e
Harsh  Vaidya <harshvaidya432004@gmail.com>
2024-01-09 18:11:36 +0530

    site updated

--- Home.py
+++ Home.py
@@ -4,9 +4,9 @@ import streamlit as st
 
 st.set_page_config(page_title='Attendance System',layout='wide')
 
-st.header('Attendance System using Face Recognition')
+st.header('Attendance System using Face Recognition ')
 
-with st.spinner("Loading Models and Conneting to Redis db ..."):
+with st.spinner("Developed by Harsh Vaidya"):
     import face_rec
     
 st.success('Model loaded sucesfully')
